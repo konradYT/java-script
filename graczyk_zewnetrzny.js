0f5e110769d12c81c73cca6aa58d64aeded90f31
konradYT <66628806+konradYT@users.noreply.github.com>
2024-10-15 18:56:58 +0200

    Update graczyk_zewnetrzny.js

--- graczyk_zewnetrzny.js
+++ graczyk_zewnetrzny.js
@@ -1,5 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#21").bold().fontsize(6) + "<br>");
-document.write(document.lastModified + "<br>"); 
-
-document.write("ostatnia modyfikacja strony".fontcolor("#33").strike().fontsize(6) + "<br>"); 
-document.write(document.lastModified);
+document.write("ostatnia modyfikacja strony".fontcolor("#21").fontsize(6) + "<br>");
+document.write(document.lastModified.strike()); 
